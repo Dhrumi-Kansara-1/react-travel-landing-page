ed218455a7dfaa433c4963e7db8162d47f69046e
Dhrumi-Kansara-1 <154498326+Dhrumi-Kansara-1@users.noreply.github.com>
2024-01-14 12:08:47 +0530

    complete

--- src/data.js
+++ src/data.js
@@ -1,25 +1,30 @@
+import tour1Img from "./assets/images/tour-1.jpeg"
+import tour2Img from "./assets/images/tour-2.jpeg"
+import tour3Img from "./assets/images/tour-3.jpeg"
+import tour4Img from "./assets/images/tour-4.jpeg"
+
 const pageLinks = [
   {
     id: 1,
     href: "#home",
-    text:"home"
-  }, 
+    text: "home",
+  },
   {
     id: 2,
     href: "#about",
-    text:"about"
+    text: "about",
   },
   {
     id: 3,
     href: "#services",
-    text:"services"
+    text: "services",
   },
   {
     id: 4,
     href: "#tours",
-    text:"tours"
-  }, 
-]
+    text: "tours",
+  },
+];
 const socialLinks = [
   {
     id: 1,
@@ -36,6 +41,69 @@ const socialLinks = [
     href: "https://www.twitter.com",
     icon: "fab fa-squarespace",
   },
-]
- 
-export { pageLinks, socialLinks }
+];
+const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+const tours = [
+  {
+    id: 1,
+    img:tour1Img,
+    date:"august 26th, 2020",
+    title:"Tibet Adventure",
+    text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location:"china",
+    days:6,
+    startingPrice:2100
+
+  },
+  {
+    id: 2,
+    img:tour2Img,
+    date:"october 1th, 2020",
+    title:"best of java",
+    text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location:"indonesia",
+    days:11,
+    startingPrice:1400,
+    },
+  {
+    id: 3,
+    img:tour3Img,
+    date:"september 15th, 2020",
+    title:"explore hong kong",
+    text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location:"hong kong",
+    days:8,
+    startingPrice:5000,
+    },
+  {
+    id: 4,
+    img:tour4Img,
+    date:"december 5th, 2019",
+    title:"kenya highlights",
+    text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location:"kenya",
+    days:20,
+    startingPrice:3300,
+    },
+];
+
+export { pageLinks, socialLinks, services, tours };
